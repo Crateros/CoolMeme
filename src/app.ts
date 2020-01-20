be00ba7f312484460b06721dd17975499a125513
Donnie <dsvene@gmail.com>
2020-01-19 19:47:57 -0800

    Add eslint to project

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
-import express = require('express');
+import express = require('express')
 
 const app: express.Application = express();
 const port = 3000;
 
 app.get('/', (request, response) => {
-    response.send('Hello from Server Land!');
+  response.send('Hello from Server Land!');
 });
 
-app.listen(port, () => {
-    return console.log('I am alive! Woohoo! Application listening on port 3000...')
-});
+app.listen(port, () => console.log('I am alive! Woohoo! Application listening on port 3000...'));
